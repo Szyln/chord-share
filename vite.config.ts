9ef3ebe1cac78cbbe24a502d6415492b91ae75a1
Sz <szyen.lin@gmail.com>
2026-06-07 09:45:48 +0800

    deploy for cloudflare page

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/chord-share-tool/', // 設定 GitHub Pages 的 Repository 名稱
 })
